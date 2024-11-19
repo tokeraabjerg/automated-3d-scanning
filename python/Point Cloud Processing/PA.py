b6d66022efc4705da251910e742f6a2a690a8056
MDP <mdpe22@student.aau.dk>
2024-11-19 09:13:37 +0100

    Overhauled script structure. Currently, functional script to take either an input rotation, or do RANSAC based IA. Followed by standard Nearest-Neighbor point association/coorrespondence. Then a simple Point-to-Plane ICP. Acceptable PCs are then merged.
    Main function accepts a lists of point clouds and a list of rotations.

--- python/Point Cloud Processing/PA.py
+++
@@ -1,9 +0,0 @@
-# This program shall take two inputs, TARGET and REFERENCE, which are both pre-processed point clouds.
-# The program shall return the the TARGET_COR, which contains the 
-
-# Likely heuristics: Normal/Normal estimation. Max dist. 
-# First, a base program, with Max Dist. heuristic
-
-# Load the point clouds
-
-# 
