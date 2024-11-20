13f95b63eeea7e4f1b7870ff97ad11ca6b9f4f95
MDP <mdpe22@student.aau.dk>
2024-11-20 09:01:05 +0100

    Updates

--- ASSIGNMENTS.md
+++ ASSIGNMENTS.md
@@ -11,21 +11,24 @@ User interface, OPEN: @tokeraabjerg
 
 Documentation, GITHUB integration, OPEN: @tokeraabjerg
 
-PCP: Point Cloud Processing, OPEN:
+PCP: Point Cloud Processing, OPEN: @MDP
 Master, which calls slave programs, IA, PA, EM, and PM
 
-IA: Initial Alignment Estimate, OPEN: @SMarius001 ?
+IA: Initial Alignment Estimate
 
 Start of iterative loop:
-PA: Point Association, Heuristics, OPEN: @MDP
+PA: Point Association, Heuristics
 
-EM: Error Metric Reduction, Point-to-Plane, OPEN:
+EM: Error Metric Reduction, Point-to-Plane
 End of iterative loop.
 
-PM: Point cloud merging, OPEN:
+PM: Point cloud mergin
 *Merge transformed point clouds*
 
-Quality Control comparison, OPEN:
+Quality Control comparison, OPEN: @Emil
 *Consists of converting CAD model to point cloud, running PA, and computing average distance*
 
+Scanner communication: @SMarius
+
+Motor control: @tokeraabjerg, @Elbaek01
 
---
+++ python/Point Cloud Processing/DT.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+def decompose_transformation(transformation):
+    """
+    Udtrækker translation og rotation fra en 4x4 transformationsmatrix.
+
+    Args:
+        transformation (np.ndarray): 4x4 transformationsmatrix.
+
+    Returns:
+        dict: En dictionary med 'translation' (x, y, z) og 'rotation' (roll, pitch, yaw i grader).
+    """
+    # Kontroller input
+    if transformation.shape != (4, 4):
+        raise ValueError("Transformationsmatrixen skal være 4x4.")
+
+    # Udtræk translation (de sidste tre elementer i den fjerde kolonne)
+    translation = transformation[:3, 3]
+
+    # Udtræk rotationsdelen (de første tre rækker og kolonner)
+    rotation_matrix = transformation[:3, :3]
+
+    # Beregn Euler-vinkler fra rotationsmatrixen (roll, pitch, yaw)
+    sy = np.sqrt(rotation_matrix[0, 0]**2 + rotation_matrix[1, 0]**2)
+
+    singular = sy < 1e-6  # Tjek for singularitet
+
+    if not singular:
+        roll = np.arctan2(rotation_matrix[2, 1], rotation_matrix[2, 2])
+        pitch = np.arctan2(-rotation_matrix[2, 0], sy)
+        yaw = np.arctan2(rotation_matrix[1, 0], rotation_matrix[0, 0])
+    else:
+        roll = np.arctan2(-rotation_matrix[1, 2], rotation_matrix[1, 1])
+        pitch = np.arctan2(-rotation_matrix[2, 0], sy)
+        yaw = 0
+
+    # Konverter rotation fra radianer til grader
+    roll = np.degrees(roll)
+    pitch = np.degrees(pitch)
+    yaw = np.degrees(yaw)
+
+    return {
+        "translation": tuple(translation),
+        "rotation": (roll, pitch, yaw),
+    }
