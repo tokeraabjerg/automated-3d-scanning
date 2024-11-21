f75dce8523fd8c50e428fa0975dadb9b4043d028
OlaVik <ovikdal@live.no>
2024-11-21 14:11:46 +0100

    Tilføjet to nye script og forbedret funktionen for at konvertere STL til punktsky

--- python/Point Cloud Processing/STP.py
+++ python/Point Cloud Processing/STP.py
@@ -7,6 +7,7 @@ def STL_to_pointcloud(stl_path,number_of_points):
     mesh = o3d.io.read_triangle_mesh(stl_path)
     mesh.compute_vertex_normals()
     pcd = mesh.sample_points_uniformly(number_of_points)
+    pcd = mesh.sample_points_poisson_disk(number_of_points=500, pcl=pcd)
     o3d.visualization.draw_geometries([pcd])
 
     return pcd
